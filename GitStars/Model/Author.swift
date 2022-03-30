4cde91dfd3e880ed5c65d939f3af494b6b278f51
Jady Linnit <linnitjady@gmail.com>
2022-03-30 18:29:53 -0300

    FEAT: created models

---
+++ GitStars/Model/Author.swift
@@ -0,0 +1,15 @@
+//
+//  ModelAuthor.swift
+//  GitStars
+//
+//  Created by SP10472 on 29/03/22.
+//
+
+import Foundation
+
+// MARK: - Author
+struct Author: Codable {
+    let login: String
+    let id: Int
+    let name: String
+}
